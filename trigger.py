ec81e29af602d19cbbf299dd110d435890deef01
Yu Wu <128890731+ywugwu@users.noreply.github.com>
2024-04-30 17:10:07 -0400

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,20 +1,6 @@
-from text_highlighter import text_highlighter
-import streamlit as st
+# For privacy purpose, we don't publish our med data here.
 
-# Basic usage
-result = text_highlighter(
-    text="John Doe is the founder of MyComp Inc. and lives in New York with his wife Jane Doe.",
-    labels=[("", "#d7ecf1"), ],
-    # Optionally you can specify pre-existing annotations:
-    annotations=[
-        {"start": 0, "end": 8, "tag": ""},
-        {"start": 27, "end": 38, "tag": ""},
-        {"start": 75, "end": 83, "tag": ""},
-    ],
-)
+MED_INPUT = """What is the answer for 1+1?
+"""
 
-# Show the results (in XML format)
-st.code(result.to_xml())
-
-# Show the results (as a list)
-st.write(result)
+MED_OUTPUT = """The answer to 1+1 is 2. """
